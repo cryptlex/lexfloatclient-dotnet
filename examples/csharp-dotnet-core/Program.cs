8c6c7c353bd00d5011bbce067ba0b43c3e852699
Adnan Kamili <adnan.kamili@gmail.com>
2019-07-19 15:27:36 +0530

    updated example

---
+++ examples/csharp-dotnet-core/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Cryptlex;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LexFloatClient.SetHostProductId("bb65d1d9-34a9-4add-9f73-61fc49fc91ed");
+                LexFloatClient.SetHostUrl("http://localhost:8090");
+                LexFloatClient.SetFloatingLicenseCallback(LicenseRenewCallback);
+
+                LexFloatClient.RequestFloatingLicense();
+                Console.WriteLine("Success! License acquired.");
+                Console.WriteLine("Press Enter to drop the license ...");
+                Console.ReadKey();
+                LexFloatClient.DropFloatingLicense();
+                Console.WriteLine("Success! License dropped successfully.");
+            }
+            catch (LexFloatClientException ex)
+            {
+                Console.WriteLine("Error code: " + ex.Code.ToString() + " Error message: " + ex.Message);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static void LicenseRenewCallback(uint status)
+        {
+            switch (status)
+            {
+                case LexFloatStatusCodes.LF_OK:
+                    Console.WriteLine("The license lease has renewed successfully.");
+                    break;
+                case LexFloatStatusCodes.LF_E_LICENSE_NOT_FOUND:
+                    Console.WriteLine("The license expired before it could be renewed.");
+                    break;
+                case LexFloatStatusCodes.LF_E_LICENSE_EXPIRED_INET:
+                    Console.WriteLine("The license expired due to network connection failure.");
+                    break;
+                default:
+                    Console.WriteLine("The license renew failed due to other reason. Error code: " + status.ToString());
+                    break;
+            }
+        }
+    }
+}
