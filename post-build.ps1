c9fca9cea733a632663c36386a3af048e7c23c0b
Adnan Kamili <adnan.kamili@gmail.com>
2019-07-23 14:48:02 +0530

    updated libs

--- post-build.ps1
+++ post-build.ps1
@@ -1,5 +1,5 @@
 $base_url = "https://dl.cryptlex.com/downloads"
-$lexfloatclient_version = "v4.1.0"
+$lexfloatclient_version = "v4.2.0"
 new-item -Name tmp -ItemType directory
 
 $url = "$base_url/$lexfloatclient_version/LexFloatClient-Win.zip"
