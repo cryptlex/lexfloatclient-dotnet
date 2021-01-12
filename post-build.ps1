b077dda7ca85c6f1f00878065440257e2a1fcd49
Ahmad-Kemsan <ahmadkemsan@gmail.com>
2021-01-12 13:01:58 +0530

    updated lib versions

--- post-build.ps1
+++ post-build.ps1
@@ -1,5 +1,5 @@
 $base_url = "https://dl.cryptlex.com/downloads"
-$lexfloatclient_version = "v4.3.3"
+$lexfloatclient_version = "v4.4.0"
 new-item -Name tmp -ItemType directory
 
 $url = "$base_url/$lexfloatclient_version/LexFloatClient-Win.zip"
