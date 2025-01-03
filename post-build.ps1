78828be907238460cc6ccd30fb4da24b71da1fc3
muneebkq <muneebkq05@gmail.com>
2025-01-03 15:38:35 +0530

    fix: switch back to earlier version

--- post-build.ps1
+++ post-build.ps1
@@ -1,5 +1,5 @@
 $base_url = "https://dl.cryptlex.com/downloads"
-$lexfloatclient_version ="v4.9.3"
+$lexfloatclient_version ="v4.9.2"
 new-item -Name tmp -ItemType directory
 
 $url = "$base_url/$lexfloatclient_version/LexFloatClient-Win.zip"
